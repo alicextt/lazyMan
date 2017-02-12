a2df686c30bf29ba611c380e3f8ccc678b75d622
txu <txu@godaddy.com>
2017-02-12 09:48:58 -0800

    program

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # lazyMan
+This is for a small realistic wechat challenge problem.
+
+# problem description.
+```
+input:
+lazyMan('Hank');
+output:
+Hi, This is Hank!
+
+input:
+lazyMan('Hank').sleep(10).eat('dinner');
+output:
+Hi, This is Hank!
+// wait 10 seconds
+Wake up after 10 seconds!;
+Eat dinner!
+
+input:
+lazyMan('Hank').sleepFirst(10).eat('dinner').eat('supper');
+output:
+// wait 10 seconds
+Wake up after 10 seconds!;
+Hi, This is Hank!
+Eat dinner!
+Eat supper!
+
+```
+
+# Design
+lazyMan -> chaining method ( sleep, eat)
+
+lazyMan -> use a queue to maintain the sequence of events, sleep have higher priority
+
+method invocation -> use subscribe and publish design pattern
---
+++ index.js
@@ -0,0 +1,98 @@
+(function (window){
+  const task = [];
+
+  /**
+   * subscribe - subscribe tasks to task queue
+   *
+   * @return {type}
+   */
+  function subscribe() {
+    var param = {};
+    var args = Array.prototype.slice.call(arguments);
+    if(args.length<1){
+      throw new Error('subscribe arguments is empty');
+    }
+    param.method = args[0];
+    param.args = args.slice(1);
+    if(param.method === 'sleepFirst'){
+      task.unshift(param);
+    }else{
+      task.push(param);
+    }
+  }
+
+  /**
+   * publish -execute task from task list
+   *
+   * @return {type}
+   */
+  function publish() {
+    if( task.length >0) {
+      run(task.shift());
+    }
+  }
+
+  /**
+   * run - run the task
+   *
+   * @param  {type} param description
+   * @return {type}       description
+   */
+  function run(param) {
+    var msg = param.method;
+    var args = param.args;
+    switch(msg){
+      case 'lazyMan': lazyMan.apply(null, args); break;
+      case 'eat': eat.apply(null, args); break;
+      case 'sleep':
+      case 'sleepFirst': sleep.apply(null, args); break;
+      default: ;
+    }
+  }
+
+  function LazyMan(){};
+  LazyMan.prototype.sleep = function(t) {
+    subscribe('sleep', t);
+    return this;
+  }
+
+  LazyMan.prototype.sleepFirst = function(t) {
+    subscribe('sleepFirst', t);
+    return this;
+  }
+
+  LazyMan.prototype.eat = function(str) {
+    subscribe('eat', str);
+    return this;
+  }
+
+  function lazyManlog(msg){
+    console.log(msg);
+  }
+
+  function eat(str) {
+    lazyManlog("Eat " + str + " !");
+    publish();
+  }
+
+  function lazyMan(str) {
+    lazyManlog("This is " + str + " !");
+    publish();
+  }
+
+  function sleep(t) {
+    setTimeout(function () {
+      lazyManlog('Wake up after ' + t + ' seconds!');
+      publish();
+    }, 1000*t);
+  }
+
+  window.LazyMan=function(str){
+    subscribe('lazyMan', str);
+    setTimeout(function(){
+        publish();
+    }, 0);
+    return new LazyMan();
+  }
+
+})(window);
